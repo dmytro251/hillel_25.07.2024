a741b558a9851dcbe8fddd4399bfc98f071ab3ca
dmytro251 <dzyogan9@gmail.com>
2024-07-30 01:48:08 +0300

    commit-2

--- lesson2/home_work_2.2.2.py
+++ lesson2/home_work_2.2.2.py
@@ -3,5 +3,17 @@ if _number % 10 > 0:
     return_number = _number % 10 * 10000 + _number % 100 // 10 * 1000 + _number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
     print(return_number)
 else:
-    return_number = _number % 100 // 10 * 1000 + _number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
-    print(f'{0}{return_number}')
+    if _number % 10 == 0 and _number % 100 // 10 > 0:
+      return_number=_number % 100 // 10 * 1000 +_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
+      print(f'{0}{return_number}')
+    else:
+      if _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 > 0:
+          return_number =_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
+          print(f'{0}{0}{return_number}')
+      else:
+          if _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 == 0 and _number // 1000 % 10 > 0:
+                return_number = _number // 1000 % 10 * 10 + _number // 10000
+                print(f'{0}{0}{0}{return_number}')
+          else:
+              return_number = _number % 100 // 10 * 1000 + _number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
+              print(f'{0}{0}{0}{0}{return_number}')
