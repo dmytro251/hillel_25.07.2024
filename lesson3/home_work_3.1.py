adbcf48a7373ae58b1280af041619f103b0a8258
dmytro251 <dzyogan9@gmail.com>
2024-08-05 21:48:24 +0300

    commit-4

--- lesson3/home_work_3.1.py
+++ lesson3/home_work_3.1.py
@@ -2,23 +2,19 @@ number_one = int(input("Введіть першу цифру: "))
 number_two = int(input("Введіть другу цифру: "))
 operation = input("Виберіть операцію(+,-,*,/):")
 if operation == ("+") :
-        suma = number_one + number_two
-        print(f"{number_one}+{number_two}={suma}")
+    suma = number_one + number_two
+    print(f"{number_one}+{number_two}={suma}")
+elif operation == "-":
+    vid = number_one - number_two
+    print(f"{number_one}-{number_two}={vid}")
+elif operation == "*":
+    mn = number_one * number_two
+    print(f"{number_one}*{number_two}={mn}")
+elif operation == "/" and number_two == 0 :
+    print("Ділення на нуль не можливе")
+elif operation == "/":
+    dil = number_one / number_two
+    print(f"{number_one}/{number_two}={dil}")
 else:
-    if operation == "-":
-        vid = number_one - number_two
-        print(f"{number_one}-{number_two}={vid}")
-    else:
-        if operation == "*":
-            mn = number_one * number_two
-            print(f"{number_one}*{number_two}={mn}")
-        else:
-            if operation == "/" and number_two == 0 :
-                 print("Ділення на нуль не можливе")
-            else:
-                if operation == "/":
-                    dil = number_one / number_two
-                    print(f"{number_one}/{number_two}={dil}")
-                else:
-                    print("Дія не можлива")
+    print("Дія не можлива")
 
--- lesson3/home_work_3.3.py
+++ lesson3/home_work_3.3.py
@@ -7,22 +7,19 @@ if size == 0:
     print(lst_2)
     print(lst_3)
     print(lst_final)
+elif size % 2 == 0:
+    lst_2 = lst[:size // 2]
+    lst_3 = lst[size // 2:]
+    lst_final = lst_2 + lst_3
+    print(lst_2)
+    print(lst_3)
+    print(lst_final)
+elif size % 2 != 0:
+    lst_2 = lst[: size // 2 + 1]
+    lst_3 = lst[size // 2 + 1:]
+    lst_final = lst_2 + lst_3
+    print(lst_2)
+    print(lst_3)
+    print(lst_final)
 else:
-    if size % 2 == 0:
-        lst_2 = lst[:size // 2]
-        lst_3 = lst[size // 2:]
-        lst_final = lst_2 + lst_3
-        print(lst_2)
-        print(lst_3)
-        print(lst_final)
-    else:
-        if size % 2 != 0:
-            lst_2 = lst[: size // 2 + 1]
-            lst_3 = lst[size // 2 + 1:]
-            lst_final = lst_2 + lst_3
-            print(lst_2)
-            print(lst_3)
-            print(lst_final)
-        else:
-
-                print("the end")
+    print("the end")
