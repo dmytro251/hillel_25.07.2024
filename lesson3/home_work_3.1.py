af8621b2f35cd8e24e4c6d2aca517b503401664a
dmytro251 <dzyogan9@gmail.com>
2024-08-13 07:44:19 +0300

    commit-1-1

--- lesson3/home_work_3.1.py
+++ lesson3/home_work_3.1.py
@@ -1,19 +1,26 @@
-number_one = int(input("Введіть першу цифру: "))
-number_two = int(input("Введіть другу цифру: "))
-operation = input("Виберіть операцію(+,-,*,/):")
-if operation == "+":
-    suma = number_one + number_two
-    print(f"{number_one}+{number_two}={suma}")
-elif operation == "-":
-    vid = number_one - number_two
-    print(f"{number_one}-{number_two}={vid}")
-elif operation == "*":
-    mn = number_one * number_two
-    print(f"{number_one}*{number_two}={mn}")
-elif operation == "/" and not number_two:
-    print("Ділення на нуль не можливе")
-elif operation == "/":
-    dil = number_one / number_two
-    print(f"{number_one}/{number_two}={dil}")
-else:
-    print("Дія не можлива")
+
+while True:
+    number_one = int(input("Введіть першу цифру: "))
+    number_two = int(input("Введіть другу цифру: "))
+    operation = input("Виберіть операцію(+,-,*,/):")
+
+    if operation == "+":
+        suma = number_one + number_two
+        print(f"\n{number_one}+{number_two}={suma}")
+    elif operation == "-":
+        vid = number_one - number_two
+        print(f"\n{number_one}-{number_two}={vid}")
+    elif operation == "*":
+        mn = number_one * number_two
+        print(f"\n{number_one}*{number_two}={mn}")
+    elif operation == "/" and not number_two:
+        print("\nДілення на нуль не можливе")
+    elif operation == "/":
+        dil = number_one / number_two
+        print(f"\n{number_one}/{number_two}={dil}")
+    else:
+        print("\nДія не можлива")
+
+    the_end = input("\nType y to continue or any character to exit: ")
+    if the_end != "y" or the_end != "Y":
+        break
