89c900509b3cda22dbaf95e55d27a9197f91a2ec
dmytro251 <dzyogan9@gmail.com>
2024-08-15 10:22:24 +0300

    commit-1-4

--- lesson5/home_work_5.3.py
+++ lesson5/home_work_5.3.py
@@ -1,11 +1,13 @@
 import string
 
 variable_name = input("Enter a name for the variable: ")
-finish_name = variable_name.title()
 
 for index, signs in enumerate(variable_name):
-    if signs in string.punctuation or signs == " ":
-        finish_name = finish_name.replace(signs, '')
+    if signs in string.punctuation:
+        variable_name = variable_name.replace(signs, "")
+
+finish_name = variable_name.title()
+finish_name = finish_name.replace(" ", "")
 
 if len(finish_name) > 139:
     print(f"#{finish_name[:139]}")
