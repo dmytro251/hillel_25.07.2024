1a60646b4b6e3eea8ea7ff6e6c2d6b1c2f7f5e29
dmytro251 <dzyogan9@gmail.com>
2024-08-22 21:10:58 +0300

    commit-2

--- lesson6/home_work_6.2.py
+++ lesson6/home_work_6.2.py
@@ -5,14 +5,19 @@ while True:
     minutes = _number // 60 % 60
     seconds = _number % 60
 
-    if hours // 10 <= 1 and minutes // 10 <= 1 and seconds // 10 <= 1:
-        print(days, f"day, 0{hours}:0{minutes}:0{seconds}")
-    elif minutes // 10 <= 1 and seconds // 10 <= 1:
-        print(days, f"day, {hours}:0{minutes}:0{seconds}")
-    elif seconds // 10 <= 1:
-        print(days, f"day, {hours}:{minutes}:0{seconds}")
+    if days % 10 > 1 or not days % 10:
+        days = f"{days} days"
     else:
-        print(days, f"day, {hours}:{minutes}:{seconds}")
+        days = f"{days} day"
+
+    if hours // 10 <= 1 and minutes // 10 <= 1 and seconds // 10 <= 1:
+        print(days, f", 0{hours}:0{minutes}:0{seconds}")
+    elif minutes // 10 <= 1 and seconds // 10 <= 1:
+        print(days, f", {hours}:0{minutes}:0{seconds}")
+    elif seconds // 10 <= 1:
+        print(days, f", {hours}:{minutes}:0{seconds}")
+    else:
+        print(days, f", {hours}:{minutes}:{seconds}")
 
     the_end = input("\nType y to continue or any character to exit: ")
     if the_end != "Y" and the_end != "y":
