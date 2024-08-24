9d041f048b90ad6dc082f527c4e3829de9b84dff
dmytro251 <dzyogan9@gmail.com>
2024-08-24 12:41:26 +0300

    commit-3

--- lesson6/home_work_6.2.py
+++ lesson6/home_work_6.2.py
@@ -1,23 +1,16 @@
 while True:
-    _number = int(input("Enter a number: "))
+    _number = int(input("Вкажіть кількість секунд: "))
     days = _number // 86400
     hours = _number // 3600 % 24
     minutes = _number // 60 % 60
     seconds = _number % 60
 
-    if days % 10 > 1 or not days % 10:
-        days = f"{days} days"
+    if 5 < days < 20 or not days % 10 or days % 10 in range(5, 10):
+        print(f"{days} Днів {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+    elif days % 10 in range(2, 5):
+        print(f"{days} Дні {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
     else:
-        days = f"{days} day"
-
-    if hours // 10 <= 1 and minutes // 10 <= 1 and seconds // 10 <= 1:
-        print(days, f", 0{hours}:0{minutes}:0{seconds}")
-    elif minutes // 10 <= 1 and seconds // 10 <= 1:
-        print(days, f", {hours}:0{minutes}:0{seconds}")
-    elif seconds // 10 <= 1:
-        print(days, f", {hours}:{minutes}:0{seconds}")
-    else:
-        print(days, f", {hours}:{minutes}:{seconds}")
+        print(f"{days} День {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
 
     the_end = input("\nType y to continue or any character to exit: ")
     if the_end != "Y" and the_end != "y":
