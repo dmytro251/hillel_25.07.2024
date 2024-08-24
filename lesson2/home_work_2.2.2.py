d4dae74748203b433e5e3f49f812ff1d3506a745
dmytro251 <dzyogan9@gmail.com>
2024-08-24 12:40:26 +0300

    commit-3

--- lesson2/home_work_2.2.2.py
+++ lesson2/home_work_2.2.2.py
@@ -2,19 +2,16 @@ _number = int(input("Введіть ціле 5-х значне число\n"))
 if _number % 10 > 0:
     return_number = _number % 10 * 10000 + _number % 100 // 10 * 1000 + _number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
     print(return_number)
+elif _number % 10 == 0 and _number % 100 // 10 > 0:
+    return_number=_number % 100 // 10 * 1000 +_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
+    print(f'{0}{return_number}')
+elif _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 > 0:
+    return_number =_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
+    print(f'{0}{0}{return_number}')
+elif _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 == 0 and _number // 1000 % 10 > 0:
+    return_number = _number // 1000 % 10 * 10 + _number // 10000
+    print(f'{0}{0}{0}{return_number}')
 else:
-    if _number % 10 == 0 and _number % 100 // 10 > 0:
-      return_number=_number % 100 // 10 * 1000 +_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
-      print(f'{0}{return_number}')
-    else:
-      if _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 > 0:
-          return_number =_number // 100 % 10 * 100 + _number // 1000 % 10 * 10 + _number // 10000
-          print(f'{0}{0}{return_number}')
-      else:
-          if _number % 10 == 0 and _number % 100 // 10 == 0 and _number // 100 % 10 == 0 and _number // 1000 % 10 > 0:
-                return_number = _number // 1000 % 10 * 10 + _number // 10000
-                print(f'{0}{0}{0}{return_number}')
-          else:
-              return_number = _number // 10000
-              print(f'{0}{0}{0}{0}{return_number}')
+    return_number = _number // 10000
+    print(f'{0}{0}{0}{0}{return_number}')
 
