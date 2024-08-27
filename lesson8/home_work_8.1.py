25acbbc238aca463d0de821f2b7c6e7e6b381762
dmytro251 <dzyogan9@gmail.com>
2024-08-27 15:57:21 +0300

    commit-1

---
+++ lesson8/home_work_8.1.py
@@ -0,0 +1,25 @@
+def add_one(some_list: list[int]) -> list[int]:
+    number = 0
+
+    for index, element in enumerate(some_list):
+        number += element * 10 ** (len(some_list) - index - 1)
+
+    number += 1
+    final_lst = list(range(len(str(number))))
+
+    if not number % 10 and number != 0:
+        for index, elements in enumerate(final_lst):
+            final_lst[index] = number // 10 ** (len(some_list) - index)
+            number %= 10 ** (len(some_list) - index)
+    else:
+        for index, elements in enumerate(final_lst):
+            final_lst[index] = number // 10 ** (len(some_list) - index - 1)
+            number %= 10 ** (len(some_list) - index - 1)
+    return final_lst
+
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
---
+++ lesson8/home_work_8.2.py
@@ -0,0 +1,18 @@
+import string
+
+
+def is_palindrome(text: str) -> bool:
+    for signs in string.punctuation:
+        if signs in text:
+            text = text.replace(signs, "")
+
+    text = text.replace(" ", "").lower()
+
+    return text == text[::-1]
+
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
