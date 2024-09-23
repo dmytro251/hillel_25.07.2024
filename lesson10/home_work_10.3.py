bebaf1873fecf01d5c0886897e6e764a0d6668b1
dmytro251 <dzyogan9@gmail.com>
2024-09-23 15:33:13 +0300

    commit-1-modification

--- lesson10/home_work_10.3.py
+++ lesson10/home_work_10.3.py
@@ -1,9 +1,13 @@
 def is_even(digit: int) -> bool:
-    if not digit or not digit % 2:
+    if not digit % 2:
         return True
     else:
         return False
 
+try:
+    is_even(0)
+except TypeError:
+    True
 
 assert is_even(2) == True, 'Test1'
 assert is_even(5) == False, 'Test2'
