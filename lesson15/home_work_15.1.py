ecb833f1edb79442a2bcdac9371cccfd2c4f716f
dmytro251 <dzyogan9@gmail.com>
2024-09-23 13:26:35 +0300

    commit-1

---
+++ lesson15/home_work_15.1.py
@@ -0,0 +1,46 @@
+class Rectangle:
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def get_square(self) -> int:
+        return self.width * self.height
+
+    @staticmethod
+    def finding_width_and_height(ploshcha: int):
+        if not ploshcha % 5:
+            return Rectangle(5, ploshcha // 5)
+        elif not ploshcha % 3:
+            return Rectangle(3, ploshcha // 3)
+        elif not ploshcha % 2:
+            return Rectangle(2, ploshcha // 2)
+        else:
+            return Rectangle(1, ploshcha)
+
+    def __eq__(self, other):
+        return self.get_square() == other.get_square()
+
+    def __add__(self, other):
+        return self.finding_width_and_height(self.get_square() + other.get_square())
+
+    def __mul__(self, n: int):
+        return self.finding_width_and_height(self.get_square() * n)
+
+    def __str__(self) -> str:
+        return f"width= {self.width}, height= {self.height}"
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+print("Ok")
